d2a1a52ad4a8e77fe8d76176a18479b336691bb4
Deekshith010905 <deekshithkumar.singirikonda@gmail.com>
2025-05-27 12:13:00 -0400

    add the files

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
-
+docker pull deekshith2001/simple-python-flask:1.0
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 deekshith2001/simple-python-flask:1.0
